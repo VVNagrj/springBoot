10e2c9fb8203e11534da2dfd84e4a790fa4a1a96
VVN <nagrjvv@gmail.com>
2024-01-04 23:42:30 +0530

    Swagger

--- src/main/java/com/springStarter/Topic/TopicController.java
+++ src/main/java/com/springStarter/Topic/TopicController.java
@@ -1,43 +1,47 @@
 package com.springStarter.Topic;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 @RestController
+@RequestMapping("/topics")
+@Api(tags = "Topic Controller", description = "Operations pertaining to topics")
 public class TopicController {
-	
+
 	@Autowired
 	private TopicService topicService;
-	
-	@RequestMapping("/topics")
+
+	@GetMapping
+	@ApiOperation("Get all topics")
 	public List<Topic> getAllTopics() {
 		return topicService.getTopics();
 	}
-	
-	@RequestMapping("/topics/{id}")
+
+	@GetMapping("/{id}")
+	@ApiOperation("Get a topic by ID")
 	public Topic getTopicById(@PathVariable String id) {
 		return topicService.getTopicById(id);
 	}
-	
-	@RequestMapping(method = RequestMethod.POST , value="/topics")
+
+	@PostMapping
+	@ApiOperation("Add a new topic")
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
-	
-	@RequestMapping(method = RequestMethod.PUT , value="/topics/{id}")
+
+	@PutMapping("/{id}")
+	@ApiOperation("Update a topic by ID")
 	public void updateTopic(@PathVariable String id, @RequestBody Topic topic) {
 		topicService.updateTopic(id, topic);
 	}
-	
-	@RequestMapping(method = RequestMethod.DELETE , value="/topics/{id}")
+
+	@DeleteMapping("/{id}")
+	@ApiOperation("Delete a topic by ID")
 	public void deleteTopic(@PathVariable String id) {
 		topicService.deleteTopic(id);
 	}
-
 }

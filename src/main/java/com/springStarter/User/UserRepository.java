36602ae96790a3f13a4122a6553d0988007cbafa
VVN <nagrjvv@gmail.com>
2024-01-06 20:10:50 +0530

    user

--- src/main/java/com/springStarter/User/UserRepository.java
+++ src/main/java/com/springStarter/User/UserRepository.java
@@ -1,10 +1,10 @@
-package com.springStarter.Topic;
+package com.springStarter.User;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TopicRepository extends MongoRepository<Topic,String> {
+public interface UserRepository extends MongoRepository<User,String> {
 	
 
 
---
+++ src/main/java/com/springStarter/User/UserService.java
@@ -0,0 +1,25 @@
+package com.springStarter.User;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface UserService {
+
+
+	public List<User> getUsers();
+
+    public User getUserById(String id);
+
+
+    public void addUser(User User);
+
+
+    public void updateUser(String id, User User);
+
+
+    public void deleteUser(String id);
+
+
+}

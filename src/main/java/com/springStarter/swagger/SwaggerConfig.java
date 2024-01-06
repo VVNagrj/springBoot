2426b03cb67350a93ea90303db4c09f42b75b55b
VVN <nagrjvv@gmail.com>
2024-01-06 22:16:54 +0530

    FInally

--- src/main/java/com/springStarter/swagger/SwaggerConfig.java
+++ src/main/java/com/springStarter/swagger/SwaggerConfig.java
@@ -2,20 +2,25 @@ package com.springStarter.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurerAdapter {
+public class SwaggerConfig {
 
     @Bean
     public Docket api() {
@@ -24,18 +29,39 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
                 .apis(RequestHandlerSelectors.basePackage("com.springStarter"))
                 .paths(PathSelectors.any())
                 .build()
+                .securityContexts(Collections.singletonList(securityContext()))
+                    .securitySchemes(Arrays.asList(apiKey()))
                 .apiInfo(apiDetails());
     }
 
     private ApiInfo apiDetails() {
-        return new ApiInfo(
-                "Basic Spring Application",
-                "Basic Spring Application",
-                "1.0",
-                "",
-                new Contact("Nagarajan", "https://www.linkedin.com/in/nagarajan-vv-910323108/", "nagrjvv@gmail.com"),
-                "API License",
-                "TBD-License URL",
-                Collections.EMPTY_LIST);
+        return new ApiInfoBuilder()  // Correct usage
+                .title("Basic Spring Application")
+                .description("Basic Spring Application with JWT Authentication")
+                .version("1.0")
+                .contact(new Contact("Nagarajan", "https://www.linkedin.com/in/nagarajan-vv-910323108/", "nagrjvv@gmail.com"))
+                .license("API License")
+                .licenseUrl("TBD-License URL")
+                .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        return Collections.singletonList(
+                SecurityReference.builder()
+                        .reference("JWT")
+                        .scopes(new springfox.documentation.service.AuthorizationScope[0])
+                        .build()
+        );
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
 }

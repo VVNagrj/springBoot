d7cde68c1f9534fc50034cebb242b5696f856600
VVN <nagrjvv@gmail.com>
2024-02-16 09:18:04 +0530

    EnableScheduling

--- src/main/java/com/springStarter/SpringStarterApplication.java
+++ src/main/java/com/springStarter/SpringStarterApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringStarterApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
 	public static void main(String[] args) {
--- src/main/java/com/springStarter/mongoDB/exp/Transaction/TransactionServiceImpl.java
+++ src/main/java/com/springStarter/mongoDB/exp/Transaction/TransactionServiceImpl.java
@@ -1,6 +1,7 @@
 package com.springStarter.mongoDB.exp.Transaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,30 +11,39 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Autowired
     TransactionRepository topicRepository;
-
+    @Override
     public List<Transaction> getTopics() {
         return topicRepository.findAll();
     }
 
+    @Override
     public Transaction getTopicById(String id) {
         return topicRepository.findById(id).get();
     }
 
-
+    @Override
     public void addTopic(Transaction topic) {
         topicRepository.save(topic);
     }
 
 
 
+    @Override
     public void updateTopic(String id, Transaction topic) {
         topicRepository.save(topic);
     }
 
 
+    @Override
     public void deleteTopic(String id) {
         topicRepository.deleteById(id);
     }
 
+
+    @Scheduled(cron = "0/30 * * * * ?")
+    public void myScheduledMethod() {
+        // Your logic here
+        System.out.println("Executing scheduled task...");
+    }
 
 }
